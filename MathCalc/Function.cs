e95f5d34c3279d5123975954d7e525cc32b12d53
Mark Novak <mark@m8it.io>
2020-07-15 09:00:56 +0200

    daily stuff

---
+++ MathCalc/Function.cs
@@ -0,0 +1,58 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MathCalc
+{
+    // public delegate void 
+    public class Function : MainWindow
+    {
+        private string _filePath;
+        public string FilePath
+        {
+            get { return _filePath; }
+            set { _filePath = this.FilePath; }
+        }
+
+        public List<Person> CSV_File_Reader(string s)
+        {
+            using (var reader = new StreamReader(_filePath = s))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                records = csv.GetRecords<Person>();
+                rec = records.ToList();
+            }
+
+            if (rec.Count() > 1)
+            {
+                foreach (var item in rec)
+                {
+                    MessageBox.Show($"{item.Name}\n{item.LastName}\n{item.DateOfBirth}");
+                }
+            }
+            else
+                return rec;
+            
+            return rec;
+        }
+        public void CSV_File_Writer(string s)
+        {
+            var person = new Person();
+            using (var writer = new StreamWriter(_filePath = s))
+            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            {
+
+            }
+        }
+        public List<Person> rec;
+        public IEnumerable<Person> records;
+    }
+
+}
---
+++ MathCalc/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ApplyButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool valid = true;
+            var person = new Person();
+            person.Name = FirstNameBox.ToString();
+            person.LastName = LastNameBox.ToString();
+            FirstNameReadOnlyBox.Text = FirstNameBox.Text;
+            LastNameReadOnlyBox.Text = LastNameBox.Text;
+
+            if (Calendar.SelectedDate != null)
+            {
+                person.DateOfBirth = (DateTime)Calendar.SelectedDate;
+                CalendarReadOnlyBox.Text = person.DateOfBirth.ToString();
+            }
+            else
+            {
+                valid = false;
+                MessageBox.Show("Please Enter a Date or Birth.", "Error");
+            }
+
+            if ((bool)CheckBox_CSV_Write.IsChecked && valid)
+                MessageBox.Show("Im gay");
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearAllBoxes();
+        }
+
+        public void ClearAllBoxes()
+        {
+            string str = null;
+            FirstNameBox.Text = str;
+            LastNameBox.Text = str;
+            Calendar.SelectedDate = DateTime.Now;
+            FirstNameReadOnlyBox.Text = str;
+            LastNameReadOnlyBox.Text = str;
+            CalendarReadOnlyBox.Text = str;
+            CheckBox_CSV_Write.IsChecked = false;
+        }
+
+        private void CheckBox_CSV_Write_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ReadCSVButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            var func = new Function();
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "CSV files (*.csv)|*.csv";
+
+
+            if ((bool)dialog.ShowDialog())
+            {
+                func.CSV_File_Reader(dialog.FileName);
+            }
+            if (func.rec.Count() == 1)
+            {
+                FirstNameReadOnlyBox.Text = func.rec[0].Name;
+                LastNameReadOnlyBox.Text = func.rec[0].LastName;
+                CalendarReadOnlyBox.Text = func.rec[0].DateOfBirth
+                                                      .ToString();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
